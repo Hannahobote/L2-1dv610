f2268c5175855d2a8a14a81f5a08309daac716ad
Hannah Obote <ao223ir@student.lnu.se>
2022-10-13 11:49:54 +0200

    simple facade for auth

---
+++ controller/auth.js
@@ -0,0 +1,29 @@
+import {SimpleAuth} from '../simple-auth/v2/src/controller/simpleAuth.js'
+
+export class Auth {
+  
+  constructor() {
+     this.auth = new SimpleAuth();
+  }
+
+
+  register(username, password) {
+    return this.auth.register()
+  }
+
+  signIn(username, password) {
+    this.auth.signIn(username, password)
+  }
+
+  signOut() {
+    return this.auth.signOut()
+  }
+
+  getCurrentUser() {
+    return this.auth.getCurrentSignedInUser()
+  }
+
+  getAllUsers() {
+    return this.auth.getAllUsers()
+  }
+}
